9eeb481abb68ca1864c846ba51e2453c37ecbd14
fedorsidorov <fedor.sidorov.92@yandex.ru>
2019-06-12 21:51:06 +0300

    some changes

---
+++ Depth-dose function/depth-dose.py
@@ -0,0 +1,51 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import importlib
+import os
+
+import my_functions as mf
+import my_variables as mv
+
+mf = importlib.reload(mf)
+mv = importlib.reload(mv)
+
+os.chdir(mv.sim_path_MAC + 'Depth-dose function')
+
+#%%
+def get_e_density(E0, Q, z):
+    
+    q = 1.6e-19
+    rho = 1.19
+    
+    RG = 4.6e-6 / rho * (E0 / 1e+3)**1.75
+    f = z / RG
+    lambda_f = 0.74 + 4.7*f - 8.9*f**2 + 3.5*f**3
+    
+    eps = Q/q * E0/RG * lambda_f
+    
+    return eps
+
+#%%
+z = np.linspace(0, 7e-4, 1000)
+
+eps_5  = get_e_density( 5e+3, 1e-4, z)
+eps_10 = get_e_density(10e+3, 1e-4, z)
+eps_20 = get_e_density(20e+3, 1e-4, z)
+eps_30 = get_e_density(30e+3, 1e-4, z)
+
+plt.semilogy(z[:95] * 1e+4, eps_5[:95])
+plt.semilogy(z[:317] * 1e+4, eps_10[:317])
+plt.semilogy(z * 1e+4, eps_20)
+plt.semilogy(z * 1e+4, eps_30)
+
+plt.xlim(0, 7)
+plt.ylim(1e+21, 1e+23)
+
+plt.xlabel('глубина (z), мкм')
+plt.ylabel(r'$\varepsilon$, эВ/см$^3$')
+
+plt.grid()
+plt.show()
+
+plt.savefig('depth-dose.png', dpi=300)
